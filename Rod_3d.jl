fc100b0d445bca82b74721fc829d2cb9676d4c19
Paul <paul@windows.com>
2024-06-25 16:27:26 -0400

    More Formatting

--- Rod_3d.jl
+++ Rod_3d.jl
@@ -8,16 +8,17 @@ function spin(L=2^5; Re=1000, U=1, ϵ=0.5, thk=2ϵ+√2, period=200)
     end
     # Oscillating motion and rotation
     function map(x, t)
-        β = 2π * t / period
+        β = 2π * t / period  # 360 degrees rotation over the given period
         R = SA[
-            cos(β) -sin(β);
-            sin(β) cos(β)
-        ]
+            cos(β) -sin(β) 0;
+            sin(β) cos(β) 0;
+            0 0 1
+            ]
         R * (x - SA[3L, 3L])
     end
-    Simulation((6L, 6L), (0, 0), L; U, ν=U*L/Re, body=AutoBody(sdf, map), ϵ)
+    Simulation((6L, 6L, L), (0, 0, 0), L; U, ν=U*L/Re, body=AutoBody(sdf, map), ϵ)
 end
 
 include("2D.jl")
 
-sim_gif!(spin(), duration=8, clims=(-10,10), plotbody=true, remeasure=true)
+sim_gif!(spin(), duration=8, clims=(-10, 10), plotbody=true, remeasure=true)
